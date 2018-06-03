448a376732318ba25c04ae2cad93c3026fd4e9de
srikar josyam <josyam.srikar@gmail.com>
2018-06-03 16:39:59 +0530

    "updated"

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Navbar, Button, Card, Row, Col,CardTitle,Breadcrumb,MenuItem,Icon,Chip,Slider,Slide, NavItem } from 'react-materialize'
 const Header = () => (
   <div>
-    <Navbar className='black' brand='logo' right>
+    <Navbar  brand='logo' right>
             <ul>
         <li><Link to='/'><Button floating large className='blue' waves='light' icon='home' /></Link></li>
         <li><Link to='/login'>Login</Link></li>
