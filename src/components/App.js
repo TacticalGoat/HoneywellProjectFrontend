c8e98b7ad1d99b2368c41e707bfc161b5279a0c1
srikar josyam <josyam.srikar@gmail.com>
2018-06-04 15:39:14 +0530

    "Fixed minor issue"

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import Header from './Header';
 import Main from './Main';
 import Foot from './Footer';
 import logo from './logo.svg';
-import Foot from './Footer';
 import './App.css';
 
 class App extends Component {
